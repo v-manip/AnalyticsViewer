d79a743c4e7c10236ac5815eeb2e1beedb6e2d71
Uebernerd <hesina@vrvis.at>
2014-01-17 10:35:01 +0100

    checkin

--- app/scripts/analytics.js
+++ app/scripts/analytics.js
@@ -5,118 +5,70 @@ var analytics = {
 
 	scatterPlot: function(arg){
 		console.log("Scatter");
-		/*###############################################################*/
-		/*########################### D3JS Test #########################*/
-		/*###############################################################*/
+		
+  var chart = nv.models.scatterChart()
+                .showDistX(true)
+                .showDistY(true)
+                .useVoronoi(true)
+                .color(d3.scale.category10().range())
+                .transitionDuration(300)
+                ;
 
-		//console.log(this.$el.width());
+  chart.xAxis.tickFormat(d3.format('.02f'));
+  chart.yAxis.tickFormat(d3.format('.02f'));
+  chart.tooltipContent(function(key) {
+      return '<h2>' + key + '</h2>';
+  });
+
+     var el = d3.select(arg.selector);
 		var el = d3.select(arg.selector);
 		$(arg.selector).empty();
-
 		var width = $(arg.selector).width() - analytics.margin.left - analytics.margin.right,
 			height = $(arg.selector).height() - analytics.margin.top - analytics.margin.bottom;
 
-		
-		var x = d3.scale.linear()
-		    .range([0, width]);
+			var mine = d3.csv.parse(arg.data);
+   el.append("svg")
+			.attr("display", "block")
+			.attr("width", width)
+		    .attr("height", height)
+	   .datum(convertData(mine, mine.length, 40))
+       .call(chart);
 
-		var y = d3.scale.linear()
-		    .range([height, 0]);
+  nv.utils.windowResize(chart.update);
 
-		var color = d3.scale.category10();
+  chart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });
 
-		var xAxis = d3.svg.axis()
-		    .scale(x)
-		    .orient("bottom");
+function convertData(inputData) {
+   var data = [];
+   var uniqueArray = [];
 
-		var yAxis = d3.svg.axis()
-		    .scale(y)
-		    .orient("left");
-
-		var svg = el.append("svg")
-		    .attr("width", width +analytics.margin.left +analytics.margin.right)
-		    .attr("height", height +analytics.margin.top +analytics.margin.bottom)
-		  .append("g")
-		    .attr("transform", "translate(" +analytics.margin.left + "," +analytics.margin.top + ")");
-
-			
-		d3.tsv("data.tsv", function(error, data) {
-		  data.forEach(function(d) {
-		    d.sepalLength = d.sepalLength * Math.random();
-		    d.sepalWidth = d.sepalWidth * Math.random();
-		  });
-
-		//Dynamic, random dataset
-		var numDataPoints = Math.round(10000 * Math.random());				//Number of dummy data points to create
-		var xRange = 8.0;	//Max range of new x values
-		var yRange = 8.0;	//Max range of new y values
-		for (var i = 0; i < numDataPoints; i++) {					//Loop numDataPoints times
-			var newNumber1 = Math.random() * xRange;	//New random number
-			var newNumber2 = Math.random() * yRange;	//New random number
-			var newArray = new Array();
-			newArray["petalLength"] = newNumber1;
-			newArray["petalWidth"] = newNumber2;
-			newArray["sepalLength"] = newNumber1;
-			newArray["sepalWidth"] = newNumber2;
-			newArray["species"] = Math.random() > 0.5 ? "setosa" : Math.random() < 0.5 ? "versicolor" : "virginica";
-			data.push(newArray);					//Add new number to array
+	for (i = 0; i < inputData.length; i++) {
+		if (uniqueArray.indexOf(inputData[i].id) == -1)
+		{
+			uniqueArray.push(inputData[i].id);
+			data.push({
+				key: inputData[i].id,
+				values: []
+			});
 		}
-		  
-		  
-		  x.domain(d3.extent(data, function(d) { return d.sepalWidth; })).nice();
-		  y.domain(d3.extent(data, function(d) { return d.sepalLength; })).nice();
+	}
+	 
+	for (j = 0; j < uniqueArray.length; j++) {
+		for (k = 0; k < inputData.length; k++)
+		{
+			if (inputData[k].id == uniqueArray[j])
+			{
+				data[j].values.push({
+				  x: inputData[k].val1
+				, y: inputData[k].val2
+				, size: inputData[k].val3
+				});
+			}
+		}	   
+	}
+	return data;
+}
 
-		  svg.append("g")
-		      .attr("class", "x axis")
-		      .attr("transform", "translate(0," + height + ")")
-		      .call(xAxis)
-		    .append("text")
-		      .attr("class", "label")
-		      .attr("x", width)
-		      .attr("y", -6)
-		      .style("text-anchor", "end")
-		      .text("Sepal Width (cm)");
-
-		  svg.append("g")
-		      .attr("class", "y axis")
-		      .call(yAxis)
-		    .append("text")
-		      .attr("class", "label")
-		      .attr("transform", "rotate(-90)")
-		      .attr("y", 6)
-		      .attr("dy", ".71em")
-		      .style("text-anchor", "end")
-		      .text("Sepal Length (cm)")
-
-		  svg.selectAll(".dot")
-		      .data(data)
-		    .enter().append("circle")
-		      .attr("class", "dot")
-		      .attr("r", 3.5)
-		      .attr("cx", function(d) { return x(d.sepalWidth); })
-		      .attr("cy", function(d) { return y(d.sepalLength); })
-		      .style("fill", function(d) { return color(d.species); });
-
-		  var legend = svg.selectAll(".legend")
-		      .data(color.domain())
-		    .enter().append("g")
-		      .attr("class", "legend")
-		      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-		  legend.append("rect")
-		      .attr("x", width - 18)
-		      .attr("width", 18)
-		      .attr("height", 18)
-		      .style("fill", color);
-
-		  legend.append("text")
-		      .attr("x", width - 24)
-		      .attr("y", 9)
-		      .attr("dy", ".35em")
-		      .style("text-anchor", "end")
-		      .text(function(d) { return d; });
-
-	      });
 	},
 
 	boxPlot: function(arg){
@@ -134,23 +86,9 @@ var analytics = {
 		    max = -Infinity;
 
 		
-
-		d3.csv("FSC_filtered_new.csv", function(error, csv) {
-		  var data = [];
-
-		  csv.forEach(function(x) {
-		    var e = Math.floor(x.Index - 1),
-		        //r = Math.floor(x.Run - 1),
-		        s = Math.floor(Math.round(Math.random() * 50.0) + x.Value * Math.random()),
-		        d = data[e];
-		    if (!d) d = data[e] = [s];
-		    else d.push(s);
-		    if (s > max) max = s;
-		    if (s < min) min = s;
-		  });
-
-		  
-
+		var mine = d3.csv.parse(arg.data);
+		var data = convertData(mine);
+		
 	  	var chart = d3.box()
 		    .whiskers(iqr(1.5))
 		    .width(width/data.length - box_separation*2)
@@ -168,8 +106,39 @@ var analytics = {
 		    .append("g")
 		      .attr("transform", "translate(" + box_separation + "," + analytics.margin.top + ")")
 		      .call(chart);
-		});
 
+
+
+
+function convertData(inputData) {
+	var data = [];
+	var uniqueArray = [];
+
+	for (i = 0; i < inputData.length; i++) {
+		if (uniqueArray.indexOf(inputData[i].id) == -1)
+		{
+			uniqueArray.push(inputData[i].id);
+		}
+	}
+	 
+	for (j = 0; j < uniqueArray.length; j++) {
+		for (k = 0; k < inputData.length; k++)
+		{
+			if (inputData[k].id != uniqueArray[j]) continue;
+		    var e = j,
+		        s = parseFloat(inputData[k].val1),
+		        d = data[e];
+		    if (!d) d = data[e] = [s];
+		    else d.push(s);
+		    if (s > max) max = s;
+		    if (s < min) min = s;
+		}	   
+	}
+	return data;
+}
+		
+		
+		
 	},
 
 	hovPlot: function(arg){
@@ -181,10 +150,39 @@ var analytics = {
 		console.log("parallelsPlot");
 		var el = d3.select(arg.selector);
 		$(arg.selector).empty();
+
+	   var traits = [];
+	   var uniqueArray = [];
+
+
+
+		var mine = d3.csv.parse(arg.data);	
+		for (i = 0; i < mine.length; i++) {
+			if (uniqueArray.indexOf(mine[i].id) == -1)
+			{
+				uniqueArray.push(mine[i].id);
+			}
+		}
+
+		var L = mine.length;
+		var firstId = 0;
+		//for (var i = 0; i < L; i++) {
+			//var obj = mine[i];
+			var obj = mine[0];
+			for (var j in obj) {
+			    if (firstId != 0)
+				{
+					traits.push(j);//alert(j);
+				}
+				else
+				{
+					firstId = 1;
+				}
+			}
+		//}		
 		
-		
-		var species = ["setosa", "versicolor", "virginica"],
-		    traits = ["sepal length", "petal length", "sepal width", "petal width"];
+		var species = uniqueArray;//["setosa", "versicolor", "virginica"],
+		//traits = ["sepal length", "petal length", "sepal width", "petal width"];
 
     	var	width = $(arg.selector).width() - analytics.margin.left - analytics.margin.right,
 			height = $(arg.selector).height() - analytics.margin.top - analytics.margin.bottom;
@@ -200,19 +198,17 @@ var analytics = {
 
 		var svg = el.append("svg:svg")
 			.attr("class", "svg")
+			.attr("display", "block")
+			.attr("width", "100%")
+		    .attr("height", "100%")
 		  .append("svg:g")
 		    .attr("transform", "translate(" + analytics.margin.left + "," + analytics.margin.top + ")");
 
 
-		d3.csv("iris.csv", function(flowers) {
-
 		  // Create a scale and brush for each trait.
 		  traits.forEach(function(d) {
-		    // Coerce values to numbers.
-		    //flowers.forEach(function(p) { p[d] = +p[d]* Math.random(); });
-
 		    y[d] = d3.scale.linear()
-		        .domain(d3.extent(flowers, function(p) { return p[d]; }))
+		        .domain(d3.extent(mine, function(p) { return p[d]; }))
 		        .range([height, 0]);
 
 		    y[d].brush = d3.svg.brush()
@@ -220,6 +216,7 @@ var analytics = {
 		        .on("brush", brush);
 		 });
 
+		 
 		  // Add a legend.
 		  var legend = svg.selectAll("g.legend")
 		      .data(species)
@@ -234,16 +231,16 @@ var analytics = {
 		  legend.append("svg:text")
 		      .attr("x", 12)
 		      .attr("dy", ".31em")
-		      .text(function(d) { return "Iris " + d; });
+		      .text(function(d) { return "Item " + d; });
 
 		  // Add foreground lines.
 		  foreground = svg.append("svg:g")
 		      .attr("class", "foreground")
 		    .selectAll("path")
-		      .data(flowers)
+		      .data(mine)
 		    .enter().append("svg:path")
 		      .attr("d", path)
-		      .attr("class", function(d) { return d.species; });
+		      .attr("class", function(d) { return d.id; });
 
 		  // Add a group element for each trait.
 		  var g = svg.selectAll(".trait")
@@ -257,6 +254,7 @@ var analytics = {
 		      .on("drag", drag)
 		      .on("dragend", dragend))*/;
 
+			  
 		  // Add an axis and title.
 		  g.append("svg:g")
 		      .attr("class", "axis")
@@ -288,10 +286,10 @@ var analytics = {
 		  function dragend(d) {
 		    x.domain(traits).rangePoints([0, w]);
 		    var t = d3.transition().duration(500);
-		    t.selectAll(".trait").attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+		    t.selectAll(".trait").att*r("transform", function(d) { return "translate(" + x(d) + ")"; });
 		    t.selectAll(".foreground path").attr("d", path);
 		  }*/
-		});
+		//});
 
 		// Returns the path for a given data point.
 		function path(d) {
